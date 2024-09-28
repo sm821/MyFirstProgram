b056f12129047ffa0bb254861cb409f6f0a02c1d
sm821 <sm82190@email.vccs.edu>
2024-09-28 10:49:47 -0400

    Java first homework

---
+++ MyFirstProgram/src/MyFirstProgram/MyFirstProgram.java
@@ -0,0 +1,11 @@
+package MyFirstProgram;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
